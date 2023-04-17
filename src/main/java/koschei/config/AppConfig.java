3a38eebde5ddda9e3d6cf14a8a359d5672676d27
ScOutOfff <scoutoff@ya.ru>
2023-04-17 16:48:42 +0300

    Test

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -9,7 +9,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
+    public static void main(String[] args) {
 
+    }
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
